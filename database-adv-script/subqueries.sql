624fbcb59e0314bc5c63a5f5fe18a7006054d709
Kingsley Orji <mazikinzy@outlook.com>
2025-11-17 06:12:20 +0000

    Delete database-adv-script/subqueries.sql

--- database-adv-script/subqueries.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM review
-WHERE (SELECT AVG(rating) FROM review) > 4;
-
--- Write a correlated subquery to find users who have made more than 3 bookings.
-SELECT CONCAT(first_name, '', last_name) as Users FROM users
-WHERE (SELECT user_id FROM booking
-GROUP BY user_id
-HAVING COUNT(user_id) > 1);
