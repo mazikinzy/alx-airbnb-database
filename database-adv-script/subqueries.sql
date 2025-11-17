1fd8f8573aaef6d095e20add5b3708df6c105be4
Kingsley Orji <mazikinzy@outlook.com>
2025-11-17 06:12:39 +0000

    Add files via upload

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,8 @@
+SELECT * FROM review
+WHERE (SELECT AVG(rating) FROM review) > 4.0;
+
+-- Write a correlated subquery to find users who have made more than 3 bookings.
+SELECT CONCAT(first_name, '', last_name) as Users FROM users
+WHERE (SELECT user_id FROM booking
+GROUP BY user_id
+HAVING COUNT(user_id) > 1);
