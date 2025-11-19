d9dfaaa8b4f650375e9bf4441e9a3a5b3f91dcd1
Kingsley Orji <mazikinzy@outlook.com>
2025-11-19 05:34:33 +0000

    Add files via upload

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,33 @@
+EXPLAIN
+SELECT b.booking_id,
+	   b.start_date,
+       b.end_date,
+       b.total_price,
+       b.booking_status,
+       b.created_at,
+       
+       u.first_name,
+       u.last_name,
+       
+       p.property_name,
+       p.location,
+       p.price_per_night,
+       
+       pay.amount,
+       pay.payment_date,
+       pay.payment_method
+       
+FROM booking AS b
+LEFT JOIN users as u
+ON b.user_id = u.user_id
+LEFT JOIN property as p
+ON b.property_id = p.property_id
+LEFT JOIN payment as pay
+ON b.booking_id = pay.booking_id;
+
+SELECT b.booking_id, b.start_date, b.end_date, b.total_price,
+	   b.booking_status FROM booking AS b
+WHERE b.booking_id = user_id AND b.booking_id = property_id;
+
+
+show indexes from booking;
