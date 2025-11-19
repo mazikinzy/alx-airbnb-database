d80e217313f931ff3c4868aa986c6ef7c67b4b46
Kingsley Orji <mazikinzy@outlook.com>
2025-11-19 03:14:40 +0000

    Delete database-adv-script/database_index.sql

--- database-adv-script/database_index.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE INDEX user_id_last_name_email_idx
-ON users(user_id, last_name, email);
-
-CREATE INDEX property_host_propertyname_idx
-ON property(property_id, host_id, property_name);
-
-CREATE INDEX booking_property_user_idx
-ON booking(booking_id, property_id, user_id);
